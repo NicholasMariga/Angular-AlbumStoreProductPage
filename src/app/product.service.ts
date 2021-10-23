149357ca73912b2e4c40030e7b73745162e276dd
Nicholas Mariga <dolphnikoz@gmail.com>
2021-10-23 23:50:50 +0300

    Learning Angular

---
+++ src/app/product.service.ts
@@ -0,0 +1,28 @@
+
+import { Http, Response } from '@angular/http';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
+
+@Injectable()
+export class ProductService {
+
+  private _albumUrl = '../assets/album.json';
+
+  constructor(private _http: Http) { }
+
+
+  //That getAlbum() method should return the result of a call 
+  //to this._http.get(), and that get() method should take _albumUrl as a parameter.
+  // getAlbum(id: number): Observable<> {
+
+  //   return this._http.get(_albumUrl).map(response => response.json());
+
+  // }
+
+}
+
+
+
