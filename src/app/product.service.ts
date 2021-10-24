018b8ce52a41a5527cb1ed6341f3b41d2c15c95b
Nicholas Mariga <dolphnikoz@gmail.com>
2021-10-24 14:38:39 +0300

    Developing an album web App

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,6 +3,7 @@ import { Http, Response } from '@angular/http';
 import { Injectable } from '@angular/core';
 import 'rxjs/add/operator/map';
 import { Album } from './album';
+import { Product } from './product'
 import { Observable } from 'rxjs/Observable';
 
 
@@ -10,6 +11,7 @@ import { Observable } from 'rxjs/Observable';
 export class ProductService {
 
   private _albumUrl = '../assets/album.json';
+  private _productsUrl = '../assets/products.json';
 
   constructor(private _http: Http) { }
 
@@ -18,6 +20,10 @@ export class ProductService {
   //to this._http.get(), and that get() method should take _albumUrl as a parameter.
   getAlbum(id: number): Observable<Album> {
     return this._http.get(this._albumUrl).map((response) => <Album>response.json());
+  }
+
+  getProducts(): Observable<Product[]>{
+    return this._http.get(this._productsUrl).map((response) =><Product[]>response.json());
   }
 
 }
